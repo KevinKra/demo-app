4029b29c5415c115f71d37f782979a0e5abbdd6c
KevinKra <kevin.krato@stackhawk.com>
2022-05-02 18:25:16 -0600

    get basic styles on page for Paper

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,17 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import styled from "@emotion/styled";
+import { Paper, Typography } from "@mui/material";
 
 const Dashboard = () => {
-  return <Typography>Hello Wooooorld</Typography>;
+  return (
+    <StyledPaper elevation={2} width="500" padding="1rem">
+      <Typography>Hello Wooooorld</Typography>
+    </StyledPaper>
+  );
 };
 
 export default Dashboard;
+
+const StyledPaper = styled(Paper)<{ width: string; padding: string }>`
+  padding: ${(props) => props.padding};
+  width: ${(props) => props.width}px;
+`;
